9c321890860a877702c7ccbf58f086baea6910fc
root <bcddmp>
2020-06-01 17:57:25 -0700

    up

---
+++ firmware/vcan_mkmc.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=vcan_mkmc.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/vcan_mkmc.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=vcanmkmc.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/vcanmkmc.x.tar
