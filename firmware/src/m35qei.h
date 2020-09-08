43f30ec342ab16b33dc792d1612ef4e573ca52dd
root <bcddmp>
2020-09-08 16:36:46 -0700

    up

--- firmware/src/m35qei.h
+++ firmware/src/m35qei.h
@@ -36,7 +36,7 @@ extern "C" {
 #define hpwm_high_duty	duty_max-1
 #define hpwm_low_duty	1
 
-#define motor_error_stop	10
+#define motor_error_stop	ENCODER_PULSES_PER_REV/2000
 #define motor_error_coast	100
 #define motor_error_knee	200
 
